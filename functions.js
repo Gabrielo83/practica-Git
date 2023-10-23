21f7f793c984cbc4ab6aa1927955c0a28b572aaf
Hugo Gabriel Caamaño <gabrielc83@mail.com>
2023-10-22 21:55:08 -0300

    feat: Se agregan funciones de calculo de área de rectángulo y triángulos

--- functions.js
+++ functions.js
@@ -2,3 +2,7 @@ const sum = (a, b) => a + b;
 const sub = (a, b) => a + b;
 const mult = (a, b) => a + b;
 const div = (a, b) => a + b;
+
+const rectangleArea = (b, h) => b * h;
+
+const triangleArea = (b, h) => (b * h) / 2;
